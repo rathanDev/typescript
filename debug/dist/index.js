5c618b89b7af2e9d11707ca9c4edda01353a043c
jana <rathan.kparam@gmail.com>
2022-09-10 14:55:39 +0800

    Convert list of objects to map

--- debug/dist/index.js
+++ debug/dist/index.js
@@ -1,10 +1,26 @@
 "use strict";
-console.log('Hello');
+console.log("Hello");
 let age = 31;
 if (age > 50) {
-    console.log('old');
+    console.log("old");
 }
 else {
-    console.log('young');
+    console.log("young");
 }
+const nums = [1, 2, 3, 4, 5];
+const nums2 = nums.map((n) => n * 2);
+console.log(nums2);
+let one = { name: "x", age: 10 };
+let two = { name: "y", age: 20 };
+let three = { name: "z", age: 30 };
+const list = [];
+list.push(one);
+list.push(two);
+list.push(three);
+console.log(list);
+const map = list.reduce((map, e) => {
+    map[e.name] = e.age;
+    return map;
+}, {});
+console.log(map);
 //# sourceMappingURL=index.js.map
--- debug/src/index.ts
+++ debug/src/index.ts
@@ -1,8 +1,45 @@
-console.log('Hello');
+console.log("Hello");
 
 let age: number = 31;
 if (age > 50) {
-    console.log('old');
+  console.log("old");
 } else {
-    console.log('young')
+  console.log("young");
 }
+
+const nums: number[] = [1, 2, 3, 4, 5];
+
+const nums2 = nums.map((n) => n * 2);
+console.log(nums2);
+
+interface I {
+  name: string;
+  age: number;
+}
+
+let one: I = { name: "x", age: 10 };
+let two: I = { name: "y", age: 20 };
+let three: I = { name: "z", age: 30 };
+
+const list: I[] = [];
+list.push(one);
+list.push(two);
+list.push(three);
+
+console.log(list);
+
+const map  = list.reduce((map:any, e:I) => {
+    map[e.name] = e.age;
+    return map;
+}, {});
+
+console.log(map)
+
+
+
+
+
+
+
+
+
