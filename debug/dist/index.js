11b9de6c6c147c973e2fc4b3780c4f6d53a0531b
jana <rathan.kparam@gmail.com>
2022-09-10 14:41:42 +0800

    Debug

--- debug/dist/index.js
+++ debug/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log('Hello');
+let age = 31;
+if (age > 50) {
+    console.log('old');
+}
+else {
+    console.log('young');
+}
+//# sourceMappingURL=index.js.map
--- debug/src/index.ts
+++ debug/src/index.ts
@@ -1,1 +1,8 @@
 console.log('Hello');
+
+let age: number = 31;
+if (age > 50) {
+    console.log('old');
+} else {
+    console.log('young')
+}
